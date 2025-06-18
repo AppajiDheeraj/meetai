357b0f2db729d84f7e5f206b8a0e9565678a5612
Appaji Nagaraja Dheeraj <dheeraj.nagaraja@gmail.com>
2025-06-18 09:54:13 +0530

    05-authentication-socials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,100 +1,16 @@
-"use client";
-import React, { useState } from "react";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { authClient } from "@/lib/auth-client";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { HomeView } from "@/modules/home/ui/views/home-view"
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-      const { 
-        data: session,
-    } = authClient.useSession() 
-
-    if (session) {
-      return (
-        <div className="flex flex-col items-center justify-center h-screen">
-          <p>Logged in as {session.user.name}</p>
-          <Button onClick={() => authClient.signOut() }
-            className="mt-4"
-            variant="outline"
-            size="lg"
-          >
-            Sign Out
-          </Button>
-        </div>
-      )
-    }
-  const onSubmit = async () => {
-    await authClient.signUp.email({
-      name,
-      email,
-      password,
-    }, {
-      onSuccess: () => {
-        window.alert("User created successfully");
-      },
-      onError: () => {
-        window.alert("Error creating user");
-      },
-    }
-    )
+const Page = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  })
+  if (!session) {
+    redirect('/sign-in');
   }
-  const onLogin = async () => {
-    await authClient.signIn.email({
-      email,
-      password,
-    }, {
-      onSuccess: () => {
-        window.alert("User signedin successfully");
-      },
-      onError: () => {
-        window.alert("Error signing user");
-      },
-    }
-    )
-  }
-
-
-  return (
-    <div>
-      <Input placeholder="name" value={name} onChange={(event) => {
-        setName(event.target.value);
-      }} />
-      <Input placeholder="email" value={email} onChange={(event) => {
-        setEmail(event.target.value);
-      }} />
-      <Input placeholder="password" type="password" value={password} onChange={(event) => {
-        setPassword(event.target.value);
-      }} />
-      <Button
-        onClick={onSubmit}
-        className="mt-4"
-        disabled={!name || !email || !password}
-        variant="outline"
-        size="lg"
-      >
-        Sign Up
-      </Button>
-
-
-      <Input placeholder="email" value={email} onChange={(event) => {
-        setEmail(event.target.value);
-      }} />
-      <Input placeholder="password" type="password" value={password} onChange={(event) => {
-        setPassword(event.target.value);
-      }} />
-      <Button
-        onClick={onLogin}
-        className="mt-4"
-        disabled={!email || !password}
-        variant="outline"
-        size="lg"
-      >
-        Sign IN
-      </Button>
-    </div>
-  );
+  return <HomeView />
 }
+
+export default Page;
