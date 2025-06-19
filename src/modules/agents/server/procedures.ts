05bb2fb07c59524ac88bdf8fc84817c881fd9fe1
Appaji Nagaraja Dheeraj <dheeraj.nagaraja@gmail.com>
2025-06-19 12:08:25 +0530

    09: Agents setup

---
+++ src/modules/agents/server/procedures.ts
@@ -0,0 +1,17 @@
+import { db } from "@/db";
+import { agents } from "@/db/schema";
+import { createTRPCRouter, baseProcedure } from "@/trpc/init";
+import { TRPCError } from "@trpc/server";
+
+export const agentsRouter = createTRPCRouter({
+  getMany: baseProcedure.query(async () => {
+    const data = await db.select().from(agents);
+    await new Promise((resolve) => {
+      setTimeout(resolve, 5000); // Simulate a delay of 5 second
+    })
+    // throw new TRPCError({
+    //   code: "BAD_REQUEST"})
+
+    return data;
+  }),
+});
